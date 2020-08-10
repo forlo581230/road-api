8532c1c07f0050c4a0528ab818a922e2f984dd23
Alex DZ <lores75634@gmail.com>
2020-08-10 16:54:49 +0800

    fix bug

--- route_post.js
+++ route_post.js
@@ -5,6 +5,7 @@ var router = express.Router();
 const RecordDB = require('./db/RecordDB');
 
 router.post('/insertRoad', async function(req, res) {
+
     RecordDB.OverSpeedRecord.insertMany(req.body.list, function(err, values) {
         try {
             if (err) {
--- test.js
+++ test.js
@@ -1,5 +1,45 @@
-var re = new RegExp('號$');
-// {address:{$regex:/\d+號/}}
-let gg = "新北市新莊區新莊路107號".replace(/\d+號/g, "");
+// var re = new RegExp('號$');
+// // {address:{$regex:/\d+號/}}
+// let gg = "新北市新莊區新莊路107號".replace(/\d+號/g, "");
 
-console.log(gg);
+// console.log(gg);
+
+let request = require('request');
+
+let newITTSNearestRoadList = [{
+    name: '忠義路一段618號',
+    city: '桃園市',
+    town: '龜山區',
+    nameType: null,
+    speedLimit: 60,
+    lat: 25.015873000000003,
+    lng: 121.3480415
+}, {
+    name: '忠義路一段618號',
+    city: '桃園市',
+    town: '龜山區',
+    nameType: null,
+    speedLimit: 60,
+    lat: 25.015873000000003,
+    lng: 121.3480415
+}];
+
+
+request({
+        method: 'POST',
+        uri: 'http://localhost:3000/post/insertRoad',
+        body: { key: "123", list: newITTSNearestRoadList },
+        json: true
+    },
+    (error, response, body) => {
+        try {
+            if (error) {
+                throw error;
+            } else {
+                console.log(response.body);
+            }
+        } catch (e) {
+            console.error(e, "insert ITTSNearestRoads err");
+        }
+    }
+);
